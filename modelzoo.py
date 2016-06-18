954a8e0b7bdc3308ddc3f9afce9c0272e0988f51
ccnlab <marcel.van.gerven@gmail.com>
2016-06-18 17:44:34 +0200

    modelzoo update

--- modelzoo.py
+++ modelzoo.py
@@ -3,6 +3,24 @@ import chainer.functions as F
 import chainer.links as L
 import numpy as np
 
+# class MLP(Chain):
+#     """
+#     Multilayer perceptron with continuous output
+#     """
+#
+#     def __init__(self, ninput, nhidden, noutput):
+#         super(MLP, self).__init__(
+#             l1=L.Linear(ninput, nhidden, wscale=np.sqrt(2)),
+#             bn1=L.BatchNormalization(nhidden),
+#             l2=L.Linear(nhidden, noutput, initialW=np.zeros((noutput, nhidden), dtype=np.float32)))
+#             bn2=L.BatchNormalization(noutput)
+#         )
+#
+#     def __call__(self, x):
+#         h = F.relu(self.bn1(self.l1(x)))
+#         y = self.bn2(self.l2(h))
+#         return y
+
 class MLP(Chain):
     """
     Multilayer perceptron with continuous output
@@ -11,13 +29,49 @@ class MLP(Chain):
     def __init__(self, ninput, nhidden, noutput):
         super(MLP, self).__init__(
             l1=L.Linear(ninput, nhidden, wscale=np.sqrt(2)),
-            l2=L.Linear(nhidden, noutput, initialW=np.zeros((noutput, nhidden), dtype=np.float32))
-        )
+            l2=L.Linear(nhidden, noutput, initialW=np.zeros((noutput, nhidden), dtype=np.float32)))
+
 
     def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        y  = self.l2(h1)
+        h = F.relu(self.l1(x))
+        y = self.l2(h)
         return y
+
+# class CNN(Chain):
+#     """
+#     Convolutional neural network
+#     """
+#
+#     def __init__(self, ninput, nframes, nhidden, noutput):
+#         super(CNN, self).__init__(
+#             # dependence between filter size and padding; output still 20x20 due to padding
+#             l1=L.Convolution2D(nframes, nhidden, 3, 1, 1),
+#             bn1=L.BatchNormalization(nhidden),
+#             l2=L.Linear(ninput[0]*ninput[1]*nhidden, noutput, initialW=np.zeros((noutput, ninput[0]*ninput[1]*nhidden), dtype=np.float32)),
+#             bn2=L.BatchNormalization(noutput)
+#         )
+#
+#     def __call__(self, x):
+#         h = F.relu(self.bn1(self.l1(x)))
+#         y = self.bn2(self.l2(h))
+#         return y
+
+class CNN(Chain):
+    """
+    Convolutional neural network
+    """
+
+    def __init__(self, ninput, nframes, nhidden, noutput):
+        super(CNN, self).__init__(
+            # dependence between filter size and padding; output still 20x20 due to padding
+            l1=L.Convolution2D(nframes, nhidden, 3, 1, 1),
+            l2=L.Linear(ninput[0]*ninput[1]*nhidden, noutput, initialW=np.zeros((noutput, ninput[0]*ninput[1]*nhidden), dtype=np.float32)))
+
+    def __call__(self, x):
+        h = F.relu(self.l1(x))
+        y = self.l2(h)
+        return y
+
 
 class RNN(Chain):
     """
@@ -37,3 +91,24 @@ class RNN(Chain):
 
     def reset(self):
         self.l1.reset_state()
+
+class CRNN(Chain):
+    """
+    Convolutional recurrent neural network
+    """
+
+    def __init__(self, ninput, nhidden, noutput):
+        super(CNN, self).__init__(
+            l1=L.Convolution2D(1, nhidden, 3, 1, 1),
+            l2=L.LSTM(ninput[0]*ninput[1]*nhidden, nhidden),
+            l3=L.Linear(nhidden, noutput, initialW=np.zeros((noutput, nhidden), dtype=np.float32))
+        )
+
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        h2 = F.relu(self.l2(h1))
+        y  = self.l3(h2)
+        return y
+
+    def reset(self):
+        self.l2.reset_state()
